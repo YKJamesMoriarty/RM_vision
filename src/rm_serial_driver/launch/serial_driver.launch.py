631a28efade1a34e21cdc22d8c4bf6d9379ca4d6
LihanChen2004 <757003373@qq.com>
2024-03-01 17:06:15 +0800

    ✨ feat(rm_serial_driver): 添加导航控制包

--- src/rm_serial_driver/launch/serial_driver.launch.py
+++ src/rm_serial_driver/launch/serial_driver.launch.py
@@ -1,9 +1,13 @@
+import os
+
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
+    config = os.path.join(
+        get_package_share_directory('rm_serial_driver'), 'config', 'serial_driver.yaml')
 
     rm_serial_driver_node = Node(
         package='rm_serial_driver',
@@ -11,6 +15,7 @@ def generate_launch_description():
         namespace='',
         output='screen',
         emulate_tty=True,
+        parameters=[config],
     )
 
     return LaunchDescription([rm_serial_driver_node])
