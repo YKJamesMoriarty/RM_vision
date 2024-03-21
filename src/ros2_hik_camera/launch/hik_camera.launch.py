ab0c83bc67ed17c55f4a77fecdce971f5279ab60
LihanChen2004 <757003373@qq.com>
2024-03-21 14:46:13 +0800

    🎈 perf(hik_camera): 添加若干个相机内参

--- src/ros2_hik_camera/launch/hik_camera.launch.py
+++ src/ros2_hik_camera/launch/hik_camera.launch.py
@@ -1,3 +1,5 @@
+import os
+
 from ament_index_python.packages import get_package_share_directory, get_package_prefix
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
@@ -6,8 +8,16 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
+    params_file = os.path.join(
+        get_package_share_directory('hik_camera'), 'config', 'camera_params.yaml')
+
+    camera_info_url = 'package://hik_camera/config/camera_info.yaml'
 
     return LaunchDescription([
+        DeclareLaunchArgument(name='params_file',
+                              default_value=params_file),
+        DeclareLaunchArgument(name='camera_info_url',
+                              default_value=camera_info_url),
         DeclareLaunchArgument(name='use_sensor_data_qos',
                               default_value='false'),
 
@@ -16,7 +26,8 @@ def generate_launch_description():
             executable='hik_camera_node',
             output='screen',
             emulate_tty=True,
-            parameters=[{
+            parameters=[LaunchConfiguration('params_file'), {
+                'camera_info_url': LaunchConfiguration('camera_info_url'),
                 'use_sensor_data_qos': LaunchConfiguration('use_sensor_data_qos'),
             }],
         )
